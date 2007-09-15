66c90e6acd47d90bdf194ce4712a176f664413a5
kana <kana@4142a4a8-9c22-0410-a14c-65946c90a037>
2007-09-15 14:15:45 +0000

    * Revise some comments.
    * Revise the style of key-combo notations:
      - Write the name of modifiers in full, e.g.:
        Yes: Alt-Ctrl-Del
        No:  A-C-Del
      - Write the name of modifiers in alphabetically order, e.g.:
        Yes: Alt-Ctrl-Del
        Yes: Ctrl-Alt-Del
      - Write the name of Ctrl keys as this.
    
    * window: Modify the way to use alternative method to minimize.
    
    * application launcher: Remove the hotkey for timidi95 -- which is rarely used.
    
    * reload_config: Show a message if it's not the first time to be called.
    
    * call ui.tray.activate after all initialization.
    
    
    git-svn-id: file:///c/cygwin/home/kana/var/svn-repos/config/trunk@363 4142a4a8-9c22-0410-a14c-65946c90a037

--- cereja/config.lua
+++ cereja/config.lua
@@ -1,5 +1,7 @@
 -- $Id$
 
+-- Hotkey for reload_config should be registered at first
+-- to be able to reload this file even if this file is failed to load.
 ui.hotkey.reset()
 ui.hotkey.register('Alt-Ctrl-Shift-Win-R', reload_config)
 
@@ -11,18 +13,14 @@ ui.hotkey.register('Win-T', ui.tray.activate)
 
 ui.tray.hotkey_add('Ctrl-J', ui.tray.CMD_ICON_MOVE_LAST)
 ui.tray.hotkey_add('Ctrl-K', ui.tray.CMD_ICON_MOVE_FIRST)
-ui.tray.hotkey_add('S-Ctrl-J', ui.tray.CMD_ICON_SHIFT_NEXT)
-ui.tray.hotkey_add('S-Ctrl-K', ui.tray.CMD_ICON_SHIFT_PREV)
+ui.tray.hotkey_add('Ctrl-Shift-J', ui.tray.CMD_ICON_SHIFT_NEXT)
+ui.tray.hotkey_add('Ctrl-Shift-K', ui.tray.CMD_ICON_SHIFT_PREV)
 
 
 -- window
-local WM_SYSCOMMAND = 274
-local SC_MINIMIZE = 0xF020
 ui.hotkey.register('Alt-Shift-M', ui.window.minimize)
-ui.hotkey.register('Alt-Shift-Win-M',
-                   function ()
-                     ui.window.post_message(NULL, WM_SYSCOMMAND, SC_MINIMIZE)
-                   end)
+ui.hotkey.register('Alt-Shift-Win-M',  -- alt method
+                   function () ui.window.minimize(NULL, true) end)
 ui.hotkey.register('Alt-Shift-X', ui.window.maximize)
 ui.hotkey.register('Alt-Shift-R', ui.window.restore)
 ui.hotkey.register('Alt-Shift-V', ui.window.maximize_vertically)
@@ -31,17 +29,17 @@ ui.hotkey.register('Alt-Shift-H', ui.window.maximize_horizontally)
 ui.hotkey.register('Alt-Shift-A', ui.window.set_always_on_top)
 ui.hotkey.register('Alt-Shift-Space', function () ui.window.set_alpha(204) end)
 
-ui.hotkey.register('Alt-Shift-Ctrl-H', ui.window.snap_to_left)
-ui.hotkey.register('Alt-Shift-Ctrl-K', ui.window.snap_to_top)
-ui.hotkey.register('Alt-Shift-Ctrl-L', ui.window.snap_to_right)
-ui.hotkey.register('Alt-Shift-Ctrl-J', ui.window.snap_to_bottom)
+ui.hotkey.register('Alt-Ctrl-Shift-H', ui.window.snap_to_left)
+ui.hotkey.register('Alt-Ctrl-Shift-K', ui.window.snap_to_top)
+ui.hotkey.register('Alt-Ctrl-Shift-L', ui.window.snap_to_right)
+ui.hotkey.register('Alt-Ctrl-Shift-J', ui.window.snap_to_bottom)
 ui.hotkey.register('Alt-Shift-Win-V',
                    function () ui.window.move('c', 'r', 0, 0) end)
 ui.hotkey.register('Alt-Shift-Win-H',
                    function () ui.window.move('r', 'c', 0, 0) end)
 
 
--- launcher
+-- application launcher
 function bluewind(keyword)
   return shell.execute(nil,
                        'C:\\cygwin\\usr\\win\\bin\\bluewind\\bluewind.exe',
@@ -58,7 +56,6 @@ ui.hotkey.register('Alt-Ctrl-J', function () bluewind('jtrim') end)
 ui.hotkey.register('Alt-Ctrl-L', function () bluewind('latest') end)
 ui.hotkey.register('Alt-Ctrl-O', function () bluewind('opera') end)
 ui.hotkey.register('Alt-Ctrl-S', function () bluewind('shell') end)
-ui.hotkey.register('Alt-Ctrl-T', function () bluewind('timidi95') end)
 
 
 
--- cereja/login.lua
+++ cereja/login.lua
@@ -10,18 +10,29 @@ require 'ui.window'
 
 
 
-function reload_config()
-  dofile(cereja.user_directory .. '\\config.lua')
+do
+  local firstp = true
+
+  function reload_config()
+    dofile(cereja.user_directory .. "\\config.lua")
+    if firstp then
+      firstp = false
+    else
+      cereja.notice_info('config.lua', 'config.lua has been reloaded.')
+    end
+  end
 end
 
 
 
 
+-- Hide minimized windows.
 local metrics = ui.window.get_minimized_metrics()
 metrics.arrange = bor(metrics.arrange, ui.window.ARW_HIDE)
 ui.window.set_minimized_metrics(metrics)
 
 
+-- Sort icons in the tray automatically.
 shell.tray.subscribe(function (event, icon_index, flags)
   if event == shell.tray.NIM_ADD then
     shell.tray.sort_icons()
@@ -30,13 +41,11 @@ shell.tray.subscribe(function (event, icon_index, flags)
 end)
 shell.tray.sort_icons()
 
-ui.tray.activate()
-
 
 if not (cereja.on_other_shellp or ui.key.pressedp('Shift')) then
   shell.run_startup_programs()
 end
-
 reload_config()
+ui.tray.activate()
 
 -- __END__
