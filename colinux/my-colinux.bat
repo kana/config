9a90bd34f2fd8f514f451b053f516eda42578dd7
kana <kana@4142a4a8-9c22-0410-a14c-65946c90a037>
2007-10-04 13:26:13 +0000

    Add the header and the footer comments.
    Modify to run shproxs.
    
    Fix the order to run the programs - run colinux-daemon at the last.
    
    
    
    
    
    git-svn-id: file:///c/cygwin/home/kana/var/svn-repos/config/trunk@485 4142a4a8-9c22-0410-a14c-65946c90a037

--- colinux/my-colinux.bat
+++ colinux/my-colinux.bat
@@ -1,10 +1,16 @@
+REM $Id$
+
 cd C:\cygwin\usr\win\bin\coLinux
+
 REM start C:\cygwin\bin\python.exe boot-checker.py
+
+IF EXIST "C:\cygwin\bin\bash.exe" (
+  C:\cygwin\bin\run.exe C:\cygwin\bin\bash.exe --login --norc -c "/usr/bin/python /usr/win/bin/coLinux/shproxs.py"
+) ELSE (
+  REM FIXME: Version number is included, it is not portable.
+  C:\cygwin\bin\run.exe C:\cygwin\usr\win\bin\Python2.5\python.exe shproxs.py
+)
+
 .\colinux-daemon.exe @my.conf -d
 
-REM IF EXIST "C:\cygwin\bin\bash.exe" (
-REM   C:\cygwin\bin\run.exe C:\cygwin\bin\bash.exe --login --norc -c "/usr/bin/python /usr/win/bin/coLinux/shproxs.py"
-REM ) ELSE (
-REM   :FIXME: Version number is included, it is not portable.
-REM   C:\cygwin\bin\run.exe C:\cygwin\usr\win\bin\Python2.5\python.exe shproxs.py
-REM )
+REM __END__
