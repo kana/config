95b8d5324c9f8172fced8a1fdc059bb94c8acea8
kana <kana@4142a4a8-9c22-0410-a14c-65946c90a037>
2007-12-29 15:55:04 +0000

    cereja/config.lua: * APPDIRS, appdir(): Move to ``application launcher'' section. * Modify all hotkeys:   - Now all of them use ``Win-''.   - Win-Ctrl-x to launch application.   - Win-Shift-x to do various action.   - Win-Alt-x to snap a window.   - Win-Alt-Shift-x to do other special action. * Win-Shift-M, Win-Shift-Alt-M: Fix the method to use (swapped). * Win-Ctrl-T: New, to launch ``timedate''.
    git-svn-id: file:///c/cygwin/home/kana/var/svn-repos/config/trunk@690 4142a4a8-9c22-0410-a14c-65946c90a037

--- cereja/config.lua
+++ cereja/config.lua
@@ -1,19 +1,5 @@
 -- $Id$
 
-APPDIRS = {
-  'C:\\cygwin\\usr\\win\\bin',
-  'C:\\Program Files',
-}
-function appdir(app)
-  for _, dir in ipairs(APPDIRS) do
-    local path = dir .. '\\' .. app
-    if os.pathexistsp(path) then
-      return path
-    end
-  end
-  error(string.format('Application "%s" does not exist.', app))
-end
-
 -- Hotkey for reload_config should be registered at first
 -- to be able to reload this file even if this file is failed to load.
 ui.hotkey.reset()
@@ -32,44 +18,60 @@ ui.tray.hotkey_add('Ctrl-Shift-K', ui.tray.CMD_ICON_SHIFT_PREV)
 
 
 -- window
-ui.hotkey.register('Alt-Shift-M', ui.window.minimize)
-ui.hotkey.register('Alt-Shift-Win-M',  -- alt method
+ui.hotkey.register('Win-Shift-M',  -- alt method
                    function () ui.window.minimize(NULL, true) end)
-ui.hotkey.register('Alt-Shift-X', ui.window.maximize)
-ui.hotkey.register('Alt-Shift-R', ui.window.restore)
-ui.hotkey.register('Alt-Shift-V', ui.window.maximize_vertically)
-ui.hotkey.register('Alt-Shift-H', ui.window.maximize_horizontally)
--- ui.hotkey.register('Alt-Shift-S', ui.window.shade)
-ui.hotkey.register('Alt-Shift-A', ui.window.set_always_on_top)
-ui.hotkey.register('Alt-Shift-Space', function () ui.window.set_alpha(204) end)
+ui.hotkey.register('Win-Shift-Alt-M',  -- default method
+                   function () ui.window.minimize(NULL, false) end)
+ui.hotkey.register('Win-Shift-X', ui.window.maximize)
+ui.hotkey.register('Win-Shift-R', ui.window.restore)
+ui.hotkey.register('Win-Shift-V', ui.window.maximize_vertically)
+ui.hotkey.register('Win-Shift-H', ui.window.maximize_horizontally)
+-- ui.hotkey.register('Win-Shift-S', ui.window.shade)
+ui.hotkey.register('Win-Shift-A', ui.window.set_always_on_top)
+ui.hotkey.register('Win-Shift-Space', function () ui.window.set_alpha(204) end)
 
-ui.hotkey.register('Alt-Ctrl-Shift-H', ui.window.snap_to_left)
-ui.hotkey.register('Alt-Ctrl-Shift-K', ui.window.snap_to_top)
-ui.hotkey.register('Alt-Ctrl-Shift-L', ui.window.snap_to_right)
-ui.hotkey.register('Alt-Ctrl-Shift-J', ui.window.snap_to_bottom)
-ui.hotkey.register('Alt-Shift-Win-V',
+ui.hotkey.register('Win-Alt-H', ui.window.snap_to_left)
+ui.hotkey.register('Win-Alt-K', ui.window.snap_to_top)
+ui.hotkey.register('Win-Alt-L', ui.window.snap_to_right)
+ui.hotkey.register('Win-Alt-J', ui.window.snap_to_bottom)
+ui.hotkey.register('Win-Alt-Shift-V',
                    function () ui.window.move('c', 'r', 0, 0) end)
-ui.hotkey.register('Alt-Shift-Win-H',
+ui.hotkey.register('Win-Alt-Shift-H',
                    function () ui.window.move('r', 'c', 0, 0) end)
 
 
 -- application launcher
+APPDIRS = {
+  'C:\\cygwin\\usr\\win\\bin',
+  'C:\\Program Files',
+}
+function appdir(app)
+  for _, dir in ipairs(APPDIRS) do
+    local path = dir .. '\\' .. app
+    if os.pathexistsp(path) then
+      return path
+    end
+  end
+  error(string.format('Application "%s" does not exist.', app))
+end
+
 function bluewind(keyword)
   return shell.execute(nil,
                        appdir('bluewind\\bluewind.exe'),
                        '/Runcommand=' .. keyword)
 end
 
-ui.hotkey.register('Alt-Ctrl-C',
+ui.hotkey.register('Win-Ctrl-C',
                    function ()
                      shell.execute('C:\\cygwin\\usr\\local\\bin\\plala.lnk')
                    end)
-ui.hotkey.register('Alt-Ctrl-H', function () bluewind('home') end)
-ui.hotkey.register('Alt-Ctrl-I', function () bluewind('irfanview') end)
-ui.hotkey.register('Alt-Ctrl-J', function () bluewind('jtrim') end)
-ui.hotkey.register('Alt-Ctrl-L', function () bluewind('latest') end)
-ui.hotkey.register('Alt-Ctrl-O', function () bluewind('opera') end)
-ui.hotkey.register('Alt-Ctrl-S', function () bluewind('shell') end)
+ui.hotkey.register('Win-Ctrl-H', function () bluewind('home') end)
+ui.hotkey.register('Win-Ctrl-I', function () bluewind('irfanview') end)
+ui.hotkey.register('Win-Ctrl-J', function () bluewind('jtrim') end)
+ui.hotkey.register('Win-Ctrl-L', function () bluewind('latest') end)
+ui.hotkey.register('Win-Ctrl-O', function () bluewind('opera') end)
+ui.hotkey.register('Win-Ctrl-S', function () bluewind('shell') end)
+ui.hotkey.register('Win-Ctrl-T', function () bluewind('timedate') end)
 
 ui.hotkey.register('Alt-Ctrl-Shift-Win-S', function ()
   shell.execute(nil,
