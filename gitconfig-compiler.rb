56b836f9581456094bb1a197934136975814db8a
kana <nicht@s8.xrea.com>
2009-10-05 00:27:23 +0900

    git: Support writing in-line shell script

--- gitconfig-compiler.rb
+++ gitconfig-compiler.rb
@@ -3,9 +3,42 @@
 
 
 
+def script_begin?(line)
+  return line =~ /<<<$/
+end
+
+def script_end?(line)
+  return line =~ />>>$/
+end
+
+def without_script_begin_marker(line)
+  return line.sub /<<<\n/, ''
+end
+
+
+
+
 def main()
+  context = :normal  # or :script
+
   ARGF.each_line do |line|
-    puts line.sub('= ~', "= #{ENV['HOME']}")
+    if context == :normal
+      if script_begin? line
+        context = :script
+        puts without_script_begin_marker(line) + %q["!$SHELL -c ' \\]
+      else
+        puts line.sub('= ~', "= #{ENV['HOME']}")
+      end
+    elsif context == :script
+      if script_end? line
+        context = :normal
+        puts %Q[' __dummy__"]
+      else
+        puts line.gsub('"', '\"').sub("\n", " \\\n")
+      end
+    else
+      raise RuntimeError, "Invalid context #{context.inspect}"
+    end
   end
   return 0
 end
