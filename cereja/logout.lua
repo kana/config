a978587207785a8db1c36dbc49acb18b7698fc98
kana <nicht@s8.xrea.com>
2010-03-19 23:44:33 +0900

    Remove unused files

--- cereja/logout.lua
+++
@@ -1,5 +0,0 @@
--- My logout.lua
-
--- ... nothing to do here.
-
--- __END__
--- colinux/my-colinux.bat
+++
@@ -1,8 +0,0 @@
-REM Assumption: CYGWIN sshd is automatically started.
-REM Without Cygwin "run" command, the cmd.exe process to execute this bat file
-REM will remain until colinux-daemon.exe quits.
-
-cd C:\cygwin\usr\win\bin\coLinux
-C:\cygwin\bin\run.exe colinux-daemon.exe @my.conf -d
-
-REM __END__
